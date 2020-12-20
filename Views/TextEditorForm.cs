f121c1f1db2cf1c630c3fff74519404a3381f17f
Vstro <64423882+Vstro@users.noreply.github.com>
2020-12-20 14:08:36 +0300

    final commit

---
+++ Views/TextEditorForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static FileManager.Services.Services;
+
+namespace FileManager
+{
+    public partial class TextEditorForm : Form
+    {
+        String Buffer { get; set; }
+
+        String FileName { get; set; }
+
+        public TextEditorForm(String fileName)
+        {
+            InitializeComponent();
+            FileName = fileName;
+            MainTextBox.Multiline = true;
+            MainTextBox.Text = LoadFileContent(fileName);
+            ToolStripMenuItem copyMenuItem = new ToolStripMenuItem("Копировать");
+            ToolStripMenuItem pasteMenuItem = new ToolStripMenuItem("Вставить");
+            CopyPasteContextMenuStrip.Items.AddRange(new[] { copyMenuItem, pasteMenuItem });
+            MainTextBox.ContextMenuStrip = CopyPasteContextMenuStrip;
+            copyMenuItem.Click += CopyMenuItem_Click;
+            pasteMenuItem.Click += PasteMenuItem_Click;
+        }
+
+        void PasteMenuItem_Click(object sender, EventArgs e)
+        {
+            MainTextBox.Paste(Buffer);
+        }
+
+        void CopyMenuItem_Click(object sender, EventArgs e)
+        {
+            Buffer = MainTextBox.SelectedText;
+        }
+
+        private void SaveToolStripButton_Click(object sender, EventArgs e)
+        {
+            SaveFileContent(FileName, MainTextBox.Text);
+        }
+
+        private void SaveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileContent(FileName, MainTextBox.Text);
+        }
+    }
+}
